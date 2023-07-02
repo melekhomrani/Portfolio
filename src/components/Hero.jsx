e9dd5c5af41814025abcf43f3906b08f1546d5f4
melekhomrani <melekhomrani87@gmail.com>
2023-07-02 01:28:35 +0100

    removed computer canvas for small devices

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,7 +26,7 @@ const Hero = () => {
         </div>
       </div>
 
-      <ComputersCanvas />
+      {(window.innerWidth > 1020) && <ComputersCanvas />}
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
